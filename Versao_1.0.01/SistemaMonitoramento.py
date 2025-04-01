10f204de8ace5ceb09358585ceb96ee3ded55f99
JOÃO PAULO FERREIRA <joao.pf11@puccampinas.edu.br>
2025-04-01 08:51:01 -0300

    Criação da nova versao do programa

---
+++ Versao_1.0.01/SistemaMonitoramento.py
@@ -0,0 +1,64 @@
+# Sistema de Monitoramento de Sustentabilidade
+print("=============================================================")
+print("                 Sistema de Monitoramento                    ")
+print("                      Sustentavel                            ")
+print("=============================================================")
+# Coleta as informções do usuário sobre sua sustentabilidade
+date = input("Digite a Data de Hoje: ")
+qtdAgua = float(input("Digite seu consumo de água (Aprox. em Litros): ")) 
+qtdEnergia = float(input("Digite seu consumo de energia (kWh): ")) 
+qtdResiduo = float(input("Digite seu consumo de resíduos não reciclados (Kg): ")) 
+porReciclado = int(input("Digite a porcentagem de resíduos reciclados (%): "))
+
+print("Digite qual o meio de transporte você usou hoje? (S/N)")
+
+publico = input("1. Transporte público(Ônibus, metrô, trem): ")
+bicicleta = input("2. Bicicleta: ")
+caminhada = input("3. Caminhada: ")
+carro = input("4. Carro (Combustível fósseis): ")
+carroEletrico = input("5. Carro elétrico: ")
+carona = input("6. Carona compartilhada(Fósseies): ")
+print("------------------------------------------------------")
+print(f"Data: {date}")
+
+# Classificão de sustentabilidade de água
+if qtdAgua < 150:
+    print("Consumo de água: Alta Sustentabilidade")
+
+elif qtdAgua < 200:
+    print("Consumo de água: Moderada Sustentabilidade")
+    
+else:
+    print("Consumo de água: Baixa Sustentabilidade")
+
+# Classificão de sustentabilidade de energia
+if qtdEnergia < 5:
+    print("Consumo de energia: Alta Sustentabilidade")
+
+elif qtdAgua < 10:
+    print("Consumo de energia: Moderada Sustentabilidade")
+
+else:
+    print("Consumo de energia: Baixa Sustentabilidade")
+
+# Classificão de sustentabilidade dos resíduos
+porNReciclado = (100 - porReciclado)
+
+if porNReciclado > 50:
+    print("Geração de Resíduos Não Recicláveis: Baixa Sustentabilidade")
+
+elif porNReciclado > 20:
+    print("Geração de Resíduos Não Recicláveis: Moderada Sustentabilidade")
+    
+else:
+    print("Geração de Resíduos Não Recicláveis: Alta Sustentabilidade")
+
+# Uso de transporte
+if (publico or bicicleta or caminhada or carroEletrico) and (carro or carona) == "S" or "s":
+    print("Uso de Transporte: Moderado Sustentabilidade")
+
+elif (publico or bicicleta or caminhada or carroEletrico) == "S" or "s":
+    print("Uso de Transporte: Alta Sustentabilidade")
+    
+elif (carro or carona) == "S" or "s":
+    print("Consumo de energia: Baixa Sustentabilidade")
